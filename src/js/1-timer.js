6c5d4ac4eb958618bb84c183e6e8fb58ec1df49e
AndriHordiichuk <166404529+AndriHordiichuk@users.noreply.github.com>
2024-12-30 02:12:24 +0200

    commit

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,16 +1,77 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
 
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css"
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css"
 
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
+    onClose(selectedDates, instance) {
+      dateInPicker = selectedDates[0].getTime()
+      button.disabled = false
     },
-  };
+};
 
+var dateInPicker = Date.now()
+const dataSelector = document.querySelector("#datetime-picker")
+const timerElement = document.querySelector('.value');
+const timerElements = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+const button = document.querySelector('[data-start]');
+let intervalId = null
 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
   
+  // Remaining days
+  const days = Math.floor(ms / day)
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour)
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute)
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second)
+  
+  return { days, hours, minutes, seconds }
+}
+
+
+const dataPick = flatpickr(dataSelector, options)
+
+button.addEventListener('click', function() {
+  button.disabled = true
+  clearInterval(intervalId)
+  if(Date.now() >= dateInPicker) {
+    iziToast.error({
+      title: 'Wrong date',
+      message: 'Please choose a date in the future'})
+      return
+  }
+  intervalId = setInterval(() => {
+    const timeArray = convertMs(dateInPicker - Date.now())
+    if (Date.now() >= dateInPicker) {
+      clearInterval(intervalId)
+      iziToast.success({
+        title: 'Done',
+        message: 'Time!'})
+    } else {
+    for (const key in timerElements) {
+      if (timerElements[key]) { 
+          timerElements[key].textContent = timeArray[key]
+      }
+    }
+  }
+  }, 1000)
+
+})
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,21 +2,38 @@ import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
-function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
+
+const form = document.querySelector('.form')
+const makePromise = ({ delay, shouldResolve = true }) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+			if(shouldResolve) {
+				resolve(`✅ Fulfilled promise in ${delay}ms`)
+			} else {
+				reject(`❌ Rejected promise in ${delay}ms`)
+			}
+		}, delay);
+  });
+};
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const delayInput = form.elements.delay
+  const stateRadios = form.elements.state
+  const delay = Number(delayInput.value)
+  let shouldResolve = true
+  for (const radio of stateRadios) {
+    if (radio.checked && radio.value === 'rejected') {
+      shouldResolve = false
+      break
+    }
   }
+
+  makePromise({ delay, shouldResolve })
+    .then(result => iziToast.success({
+      message: result}))
+    .catch(error => iziToast.error({
+      message: error}))
+    // form.reset()
+})
